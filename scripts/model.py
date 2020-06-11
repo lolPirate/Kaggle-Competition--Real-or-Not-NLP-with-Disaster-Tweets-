2605c12c54cfcdf6876de015dbe8a178ca00bc06
Akash Dhar <akshdhr117@gmail.com>
2020-06-11 12:46:57 +0530

    Updated model.py

--- scripts/model.py
+++ scripts/model.py
@@ -3,6 +3,7 @@ import os
 import re
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -37,19 +38,45 @@ X_test = tokenizer.texts_to_sequences(X_test)
 
 X_train = pad_sequences(X_train)
 X_test = pad_sequences(X_test, maxlen=34)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
 
 # Model
 model = Sequential()
 model.add(Embedding(20000, 64, input_length=34))
 model.add(LSTM(64, return_sequences=True, input_shape=(X_train.shape[1], 1)))
 model.add(Dropout(0.2))
+model.add(LSTM(64, return_sequences=True))
+model.add(Dropout(0.2))
 model.add(LSTM(96, return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(128, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='softmax'))
+model.add(LSTM(128, return_sequences=False))
+model.add(Dropout(0.3))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics = ['accuracy'])
-print(model.summary())
+#print(model.summary())
+
 # Uncomment the next line to inititate training process
-# model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+
+print(model.evaluate(X_test, y_test))
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
